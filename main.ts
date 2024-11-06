1ffb9e4d1e8a4e312020721cfb73014c52d6e04a
grzzr <grzzr@duck>
2024-11-06 10:55:48 -0300

    item surpresa

--- main.ts
+++ main.ts
@@ -6,6 +6,20 @@ let pato = sprites.create(imgPato, SpriteKind.Player)
 let bau = sprites.create(assets.image`bau_dourado`, item)
 let imgBauAberto = assets.image`bau_dourado_aberto`
 
+let objetos = [
+    assets.image`chave_prata`,
+    assets.image`coracao`,
+    assets.image`cadeado_dourado`,
+    assets.image`arco`,
+    assets.image`martelo`,
+    assets.image`anel_agua_marinha`,
+    assets.image`anel_pedra_solar`,
+    assets.image`ASAS`,
+    assets.image`espada`,
+    assets.image`flecha`,
+
+]
+
 
 pato.setPosition(120, 50)
 martelo.setPosition(100,108)
@@ -20,7 +34,14 @@ pato.setStayInScreen(true)
 scene.cameraFollowSprite(pato)
 
 sprites.onOverlap(SpriteKind.Player, item, function (jogador, umItem) {
+    if (umItem.image != imgBauAberto) {
+        let surpresa = sprites.create(Math.pickRandom(objetos), SpriteKind.Food)
+        surpresa.setPosition(32, 64)
+    }
+    
     umItem.setImage(imgBauAberto)
+    
+    
 })
 
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (jogador, ferramenta) {
